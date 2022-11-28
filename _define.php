c9484b4b455c772bff8d20b15dc67d7c63dcb98e
franck <carnet.franck.paul@gmail.com>
2022-11-28 07:57:53 +0100

    Avoid notice on new installation

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Clone Entry',
     'Make a clone of entry',
     'Franck Paul',
-    '1.2',
+    '2.0',
     [
         'requires'    => [['core', '2.24']],
         'permissions' => dcCore::app()->auth->makePermissions([
--- _install.php
+++ _install.php
@@ -17,7 +17,7 @@ if (!defined('DC_CONTEXT_ADMIN')) {
 $new_version = dcCore::app()->plugins->moduleInfo('cloneEntry', 'version');
 $old_version = dcCore::app()->getVersion('cloneEntry');
 
-if (version_compare($old_version, $new_version, '>=')) {
+if (version_compare((string) $old_version, $new_version, '>=')) {
     return;
 }
 
