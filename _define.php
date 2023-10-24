4c0249303fed32ce4943cb87f1bb7bf84ebed2df
franck <carnet.franck.paul@gmail.com>
2023-10-24 18:52:13 +0200

    New release 5.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Clone Entry',
     'Make a clone of entry',
     'Franck Paul',
-    '4.0.1',
+    '5.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
