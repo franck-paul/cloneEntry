57bfd3d0021be10886fe7b3261d965d9f2a4fb01
franck <carnet.franck.paul@gmail.com>
2024-09-12 09:54:28 +0200

    Code review

--- _define.php
+++ _define.php
@@ -25,6 +25,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=cloneEntry',
         'support'    => 'https://github.com/franck-paul/cloneEntry',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/cloneEntry/master/dcstore.xml',
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/cloneEntry/main/dcstore.xml',
     ]
 );
